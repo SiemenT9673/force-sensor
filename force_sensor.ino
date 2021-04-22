f333a8130a7bc643cd64751f9125ecfdf66b3598
SiemenT9673 <82954113+SiemenT9673@users.noreply.github.com>
2021-04-22 08:38:51 -0500

    Add files via upload

---
+++ force_sensor.ino
@@ -0,0 +1,20 @@
+int FSR_Pin =  A0;
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
+//analog pin
+void setup()
+{
+  lcd.begin(16, 2);
+}
+void loop()
+{
+  int FSRReading = analogRead(FSR_Pin);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if 
+  (FSRReading <= 400)
+  lcd.print("more food");
+  else
+  lcd.print("no more food");
+  delay(250);
+}
